e6414fdd5b5e4aad075f4a9aab208ed070156da9
Mark Lin <crepper9990224@gmail.com>
2024-09-16 00:26:21 -0500

    Coding progress?

--- sketch.js
+++ sketch.js
@@ -3,18 +3,25 @@ let repelDist;
 let canvas;
 let makeBig = false;
 let homePageOn = true;
+let pageTwoOn = true;
+let pageThreeOn = true;
 
 let buttonW, buttonH, buttonX, buttonY; 
 let d;
 
 let headerOne;
 
-let pageTwoImage;
+let pageTwoImage, pageThreeImage, pageFourImage;
+let phoneButton, taxiButton, keysButton;
 let rectAlpha = 1; //SYD: I made this a global variable so I can change it in the pageTwo function, this is for your fade in transition
 
 function preload(){
   pageTwoImage = loadImage('Images/bar.jpg');
-
+  pageThreeImage = loadImage('Images/bedRoom.jpg');
+  pageFourImage = loadImage('Images/carCrash.jpg');
+  phoneButton = loadImage('Images/phone.png');
+  taxiButton = loadImage('Images/taxiIcon.png');
+  keysButton = loadImage('Images/keyIcon.png');
 }
 
 function setup() {
@@ -40,8 +47,6 @@ function draw() {
   fill(13,30, 88);
   rect(width/2, height/2, width, height);
 
-  // fill(100, 4, 20);
-  // rect(-484, 112, 910, 60);
   if(homePageOn){
     homePage();
   } else {
@@ -55,9 +60,6 @@ function windowResized() {
   resizeCanvas(window.innerWidth, window.innerHeight);
     mouseX = -width / 2;
     mouseY = -height / 2;
-
-
-
 }
 
 function homePage(){
@@ -83,18 +85,17 @@ function homePage(){
 
   d = dist(mouseX, mouseY, buttonX, buttonY);
 
-  
-
 }
 
 
 function mousePressed(){
   if(d < buttonW/2 && d < buttonH/2){
     homePageOn = false;
-    
   }
 
-
+  if (d<buttonW/2 && d<buttonH/2){
+    pageTwoOn = false;
+  }
 }
 
 function pageTwo(){
@@ -110,40 +111,51 @@ function pageTwo(){
   //Image---
   image(pageTwoImage, width/2, height/2, width*0.9, height*0.91);
   
-    //rectangle
-    noStroke();
-    fill(13,30, 88, 0.60);
-    rect(width/2.0, height/1.10, width*0.50, height*0.10);
+  //rectangle
+  noStroke();
+  fill(13,30, 88, 0.60);
+  rect(width/2.0, height/1.10, width*0.50, height*0.10);
 
-    //Bar text
-    textAlign(CENTER, CENTER);
+  //Bar text
+  textAlign(CENTER, CENTER);
   textSize(height*0.05)
   stroke(0);
   fill(0, 0, 0);
   text("It's late and you're drunk, bar's closing soon", width/2, height/1.1);
 
-  //Phone choice button
-  let buttonS = height*0.03;
+  //Buttons for Phone/Taxi/Keys
+  let buttonS = height*0.04;
   let buttonX = width*0.1;
-  let buttonY = buttonS/2 + 15; 
+  let buttonY = buttonS/2 + 13; 
 
+  
   noStroke();
   fill(189, 18, 44);
+  ellipse(buttonX, buttonY*0.99, buttonS, buttonS);
+  image(phoneButton, width*0.1, height*0.03, width*0.04, height*0.05);
 
   //SYD: in order to click on your button you need to repeat what we did with the start button (hint: use dist function)
   // look at dist function in your home page and look at what we wrote in the mousePressed function
 
   //SYD: The reason your ellispse wasn't showing up is you had an extra number there that shouldn't be there
-  ellipse(buttonX, buttonY, buttonS, buttonS); //SYD: I created a variable called buttonS
+  // ellipse(buttonX, buttonY, buttonS, buttonS); //SYD: I created a variable called buttonS
 
   //SYD: Adding a second circle next to the other circle, using the same variables to make the code more readable
-  ellipse(width/2, buttonY, buttonS, buttonS);
+  noStroke();
+  fill(11, 50, 91);
+  ellipse(width/2, buttonY*0.99, buttonS, buttonS);
+  image(taxiButton, width*0.5, height*0.03, width*0.02, height*0.03);
 
-  ellipse(width*0.9, buttonY, buttonS, buttonS);
+  noStroke();
+  fill(4, 29, 84);
+  ellipse(width*0.9, buttonY*0.99, buttonS, buttonS);
 
   //SYD: Add the illusion of a fade in transition by changing the alpha value of a black rectangle
   fill(0, 0, 0, rectAlpha); // Made rectAlpha a global variable so I can start it at 1 and change it to 0 gradually
   rect(width/2, height/2, width, height); 
+  image(keysButton, width*0.9, height*0.03, width*0.02, height*0.029);
+
+  d = dist(mouseX, mouseY, buttonX, buttonY);
 
   //"animate" the fade in transition by decreasing the alpha value of the rectangle
   rectAlpha -= 0.01;
@@ -153,4 +165,82 @@ function pageTwo(){
     rectAlpha = 0;
   }
 
+  function pageThree() {
+      //Background
+  background(50);
+  rectMode(CENTER);
+  let rectW = width*0.9;
+  let rectH = height*0.9; 
+  noStroke();
+  rect(width/2, height/2, rectW*0.5, rectH*0.5);
+  
+  //rectangle
+  noStroke();
+  fill(13,30, 88, 0.60);
+  rect(width/2.0, height/1.10, width*0.50, height*0.10);
+  
+  //Bar text
+  textAlign(CENTER, CENTER);
+  textSize(height*0.05)
+  stroke(0);
+  fill(0, 0, 0);
+  text("You went drunk driving and got killed in a car crash", width/2, height/1.1);
+
+  //Image---
+  image(pageThreeImage, width/2, height/2, width*0.9, height*0.91);
+  }
+
+  function pageFour() {
+    background(50);
+    rectMode(CENTER);
+    let rectW = width*0.9;
+    let rectH = height*0.9; 
+    noStroke();
+    rect(width/2, height/2, rectW*0.5, rectH*0.5);
+    
+    //rectangle
+    noStroke();
+    fill(13,30, 88, 0.60);
+    rect(width/2.0, height/1.10, width*0.50, height*0.10);
+
+    //Bar text
+    textAlign(CENTER, CENTER);
+    textSize(height*0.05)
+    stroke(0);
+    fill(0, 0, 0);
+    text("You got home safely", width/2, height/1.1);
+
+    //Image---
+    image(pageFourImage, width/2, height/2, width*0.9, height*0.91);
+    }
+  
+  function pageFive()
+  {
+  background(50);
+  rectMode(CENTER);
+  let rectW = width*0.9;
+  let rectH = height*0.9; 
+  noStroke();
+  rect(width/2, height/2, rectW*0.5, rectH*0.5);
+ 
+    //Bar text
+    textAlign(CENTER, CENTER);
+    textSize(height*0.05)
+    stroke(0);
+    fill(0, 0, 0);
+    text("Always Remember, NEVER drink and drive", width/2, height/1.1);
+
+     //Return BUTTON -----------------------------
+  noStroke();
+  fill(189, 18, 44);
+  rect(buttonX, buttonY, buttonW, buttonH, height*0.04);
+
+  textAlign(CENTER, CENTER);
+  textSize(height*0.05)
+  stroke(0);
+  fill(0, 0, 100);
+  text("Start Over?", width/2, height/1.7);
+
+  d = dist(mouseX, mouseY, buttonX, buttonY);
+  }
 }
